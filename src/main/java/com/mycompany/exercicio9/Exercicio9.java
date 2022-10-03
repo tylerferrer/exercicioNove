3edb7040fc8b6405a1889f49d60293f9b70bf74b
tylerferrer <tylerlucasferrer@gmail.com>
2022-10-03 19:39:33 -0300

    adição de comentarios

--- src/main/java/com/mycompany/exercicio9/Exercicio9.java
+++ src/main/java/com/mycompany/exercicio9/Exercicio9.java
@@ -9,13 +9,16 @@ import java.util.Scanner;
 public class Exercicio9 {
 
     public static void main(String[] args) {
+//        Variaveis e scanner
         double temperaturaF, temperaturaC;
         Scanner entrada = new Scanner(System.in);
         
+//        Entrada de dados pelo usuário
         System.out.print("Digite a temperatura: ");
         temperaturaF = entrada.nextDouble();
         entrada.close();
         
+//        Operação feita para converter
         temperaturaC = 5 * ((temperaturaF-32)/9);
         
         System.out.print("A temeperatura em Celsius é: " + temperaturaC);
